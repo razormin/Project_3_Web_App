76a936e669d762195f684e8b73fa4455e2bd1a7a
Ray Min <iray.min@gmail.com>
2021-06-10 17:53:51 -0700

    code

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -135,27 +135,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # My settings
 LOGIN_URL = '/users/login/'
 
-# Heroku settings
-import os
-import django_heroku
+# Activate Django-Heroku
+
 django_heroku.settings(locals())
-
-cwd = os.getcwd()
-if cwd == '/app' or cwd[:4] == '/tmp':
-    import jd_database_url
-    DATABASES = {
-        'default':dj_database_url.config(default='postgres://localhost')
-    }
-
-    # Honor the 'X-Forwarded-Proto' header for request.is_secure().
-    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-    # Allow all host headers.
-    ALLOWED_HOSTS = ['*']
-
-    # Static asset configuration
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = 'staticfiles'
-    STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'static'),
-    )
