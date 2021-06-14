93d4063cb1d51caa82ac87247f8acf2a5a3c7917
Ray Min <iray.min@gmail.com>
2021-06-13 17:47:07 -0700

    Updated settings.py and error pages

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'learning_log.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'learning_log/templates')],
+        'DIRS': [BASE_DIR, 'learning_log/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
